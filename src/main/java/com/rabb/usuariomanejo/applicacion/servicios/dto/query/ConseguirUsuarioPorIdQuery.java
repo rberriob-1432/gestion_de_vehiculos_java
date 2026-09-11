31f877b0e7464866de40e7d1a5d0868cceaf24df
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-10 22:19:48 -0500

    Agregando query

--- src/main/java/com/rabb/usuariomanejo/applicacion/servicios/dto/query/ConseguirUsuarioPorIdQuery.java
+++ src/main/java/com/rabb/usuariomanejo/applicacion/servicios/dto/query/ConseguirUsuarioPorIdQuery.java
@@ -1,4 +1,6 @@
 package com.rabb.usuariomanejo.applicacion.servicios.dto.query;
+import jakarta.validation.constraints.NotBlank;
+public record ConseguirUsuarioPorIdQuery(@NotBlank(message = "id no debe estar en blanco") String id)
+{
 
-public class ConseguirUsuarioPorIdQuery {
 }
